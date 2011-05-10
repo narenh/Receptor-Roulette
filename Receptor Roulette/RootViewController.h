086b6b48f25d2c7d877fa0f837eeb046ae173041
Naren Hazareesingh <narenhaz@gmail.com>
2011-05-10 17:43:07 -0500

    Initial Commit

---
+++ Receptor Roulette/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Receptor Roulette
+//
+//  Created by Naren Hazareesingh on 5/10/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
