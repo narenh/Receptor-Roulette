bb977c8dff4f6768943478d4b3d957dd852aca6b
Michael Victor Zink <zuwiki@zuwiki.net>
2011-05-17 19:14:47 -0500

    Adding ultra-basic collision detection between tcells and receptors

--- Receptor Roulette/MZNHRouletteLayer.m
+++ Receptor Roulette/MZNHRouletteLayer.m
@@ -9,7 +9,6 @@
 
 // Import the interfaces
 #import "MZNHRouletteLayer.h"
-#import "CCSprite+getBounds.h"
 
 // HelloWorldLayer implementation
 @implementation MZNHRouletteLayer
@@ -154,6 +153,18 @@
 		if (cell.position.x >= (size.width + cell.contentSize.width)) {
 			[tcellSprites removeObject: cell];
 			[self removeChild: cell cleanup:YES];
+		} else {
+			for (CCSprite * rec in receptorSprites) {
+				CGRect recBox = rec.boundingBox;
+				recBox.origin = [rec.parent convertToWorldSpace: rec.position];
+				if (CGRectIntersectsRect(cell.boundingBox, recBox)) {
+					if (selSprite == cell) selSprite = nil;
+					// Dangerous: we are looping through the same array
+					[tcellSprites removeObject: cell];
+					[self removeChild: cell cleanup: YES];
+					break;
+				}
+			}
 		}
 	}
 }
