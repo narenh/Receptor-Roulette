560161515718c6cb1146ad82d9c16f56ce9596f2
Michael Victor Zink <zuwiki@zuwiki.net>
2011-06-03 14:32:38 -0500

    Adding documentation to MZNHTCellSprite.[hm]

--- Receptor Roulette/MZNHTCellSprite.h
+++ Receptor Roulette/MZNHTCellSprite.h
@@ -9,17 +9,27 @@
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 
+/** A CCSprite with additional information about TCells */
 @interface MZNHTCellSprite : CCSprite {
     NSString * peptide;
     BOOL autoreactive;
 	BOOL functional;
 }
 
+/** The name of the peptide for this cell.
+ See +[MZNHTCellSprite peptideNames] for valid peptides. */
 @property(nonatomic, retain) NSString *peptide;
+/** Autoreactive TCells should not be selected for */
 @property(nonatomic) BOOL autoreactive;
+/** Nonfunctional TCells do not have a coreceptor and should not be selected for */
 @property(nonatomic) BOOL functional;
 
+/** Generates a TCell with random peptide, autoreactivity, and functionality,
+ placing it randomly along the left side of the screen. */
 + (MZNHTCellSprite *) randomTCellSprite;
+
+/** The list of valid peptide names. Each one should have a corresponding image
+ in the Resources/T-Cells and Resources/HD groups. */
 + (NSArray *) peptideNames;
 
 @end
--- Receptor Roulette/MZNHTCellSprite.m
+++ Receptor Roulette/MZNHTCellSprite.m
@@ -10,7 +10,14 @@
 
 #define ARC4RANDOM_MAX 0x100000000
 
+
+/** The format of image files for each peptide:
+ MZNH_Tc_[peptide][functional_flag].png
+ [functional_flag] should be an underscore character (_) if
+ the TCell is not functional */
 static NSString * spriteFilenameFormat = @"MZNH_Tc_%@%@.png";
+
+/** The peptideNames array is lazily populated in +peptideNames below */
 static NSArray * peptideNames = nil;
 
 @implementation MZNHTCellSprite
@@ -18,28 +25,41 @@ static NSArray * peptideNames = nil;
 @synthesize peptide, autoreactive, functional;
 
 + (MZNHTCellSprite *) randomTCellSprite {
+	// Pick a random peptide name
 	NSUInteger i = random() % ([[MZNHTCellSprite peptideNames] count]-1);
 	NSString * peptideName = [[MZNHTCellSprite peptideNames] objectAtIndex: i];
+
+	// There is an arbitrary 7-in-20 (35%) chance a TCell is nonfunctional
 	BOOL functional = (random() % 20 > 7 ) ? YES : NO;
+
+	// The sprite is initalized with the appropriate imagename.
+	// cocos2d will handle caching sprites.
 	MZNHTCellSprite * cell = [MZNHTCellSprite spriteWithFile:
 							  [NSString stringWithFormat: spriteFilenameFormat,
 							   peptideName, (functional ? @"" : @"_")]];
 	cell.peptide = peptideName;
 	cell.functional = functional;
+
+	// Place the sprite randomly along the left edge of the screen
 	CGSize size = [[CCDirector sharedDirector] winSize];
 	cell.position = ccp(0.0, ((float)arc4random()/ARC4RANDOM_MAX) *
 						(size.height - cell.contentSize.height * 2)
 						+ cell.contentSize.height );
-    if (random()%20 < 4) {
+
+    // There is an arbitrary 4-in-20 (20%) chance that a TCell is autoreactive
+	// and therefore nonfunctional. We discolor it.
+	if (random()%20 < 4) {
         cell.autoreactive = YES;
 		cell.functional = NO;
         cell.color = ccc3(255, 120, 0);
     }
+
 	return cell;
 }
 
 + (NSArray *) peptideNames
 {
+	// Only instantiate the array if it doesn't exist yet
 	if (! peptideNames)
 		peptideNames = [[NSArray alloc] initWithObjects: @"SB", @"SG", @"SP", @"TB", @"TG", @"TP", nil];
 	return peptideNames;
